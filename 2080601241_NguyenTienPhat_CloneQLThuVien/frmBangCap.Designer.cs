00ae18d9b0fc14c7f54fff61a63caab147bd70a6
tienp <tienphat3968@gmail.com>
2024-10-10 19:42:49 +0700

    Fix Designer

--- 2080601241_NguyenTienPhat_CloneQLThuVien/frmBangCap.Designer.cs
+++ 2080601241_NguyenTienPhat_CloneQLThuVien/frmBangCap.Designer.cs
@@ -32,18 +32,19 @@
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.CbbTenbangcap = new MetroFramework.Controls.MetroComboBox();
+            this.txtTenBangCap = new MetroFramework.Controls.MetroTextBox();
             this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
             this.txtMabangcap = new MetroFramework.Controls.MetroTextBox();
             this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
             this.label1 = new System.Windows.Forms.Label();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dgvBangCap = new MetroFramework.Controls.MetroGrid();
             this.btnThem = new System.Windows.Forms.Button();
             this.btnXoa = new System.Windows.Forms.Button();
             this.btnSua = new System.Windows.Forms.Button();
             this.btnDong = new System.Windows.Forms.Button();
             this.btnLuu = new System.Windows.Forms.Button();
-            this.dgvBangCap = new MetroFramework.Controls.MetroGrid();
+            this.btnHuy = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvBangCap)).BeginInit();
@@ -51,7 +52,7 @@
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.CbbTenbangcap);
+            this.groupBox1.Controls.Add(this.txtTenBangCap);
             this.groupBox1.Controls.Add(this.metroLabel2);
             this.groupBox1.Controls.Add(this.txtMabangcap);
             this.groupBox1.Controls.Add(this.metroLabel1);
@@ -63,22 +64,43 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Thông tin bằng cấp";
             // 
-            // CbbTenbangcap
+            // txtTenBangCap
             // 
-            this.CbbTenbangcap.FormattingEnabled = true;
-            this.CbbTenbangcap.ItemHeight = 23;
-            this.CbbTenbangcap.Location = new System.Drawing.Point(122, 87);
-            this.CbbTenbangcap.Name = "CbbTenbangcap";
-            this.CbbTenbangcap.Size = new System.Drawing.Size(148, 29);
-            this.CbbTenbangcap.TabIndex = 2;
-            this.CbbTenbangcap.UseSelectable = true;
+            // 
+            // 
+            // 
+            this.txtTenBangCap.CustomButton.Image = null;
+            this.txtTenBangCap.CustomButton.Location = new System.Drawing.Point(151, 1);
+            this.txtTenBangCap.CustomButton.Name = "";
+            this.txtTenBangCap.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.txtTenBangCap.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.txtTenBangCap.CustomButton.TabIndex = 1;
+            this.txtTenBangCap.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.txtTenBangCap.CustomButton.UseSelectable = true;
+            this.txtTenBangCap.CustomButton.Visible = false;
+            this.txtTenBangCap.Lines = new string[0];
+            this.txtTenBangCap.Location = new System.Drawing.Point(173, 104);
+            this.txtTenBangCap.MaxLength = 32767;
+            this.txtTenBangCap.Name = "txtTenBangCap";
+            this.txtTenBangCap.PasswordChar = '\0';
+            this.txtTenBangCap.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.txtTenBangCap.SelectedText = "";
+            this.txtTenBangCap.SelectionLength = 0;
+            this.txtTenBangCap.SelectionStart = 0;
+            this.txtTenBangCap.ShortcutsEnabled = true;
+            this.txtTenBangCap.Size = new System.Drawing.Size(173, 23);
+            this.txtTenBangCap.TabIndex = 2;
+            this.txtTenBangCap.UseSelectable = true;
+            this.txtTenBangCap.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.txtTenBangCap.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
             // 
             // metroLabel2
             // 
             this.metroLabel2.AutoSize = true;
-            this.metroLabel2.Location = new System.Drawing.Point(26, 97);
+            this.metroLabel2.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.metroLabel2.Location = new System.Drawing.Point(44, 102);
             this.metroLabel2.Name = "metroLabel2";
-            this.metroLabel2.Size = new System.Drawing.Size(90, 19);
+            this.metroLabel2.Size = new System.Drawing.Size(116, 25);
             this.metroLabel2.TabIndex = 0;
             this.metroLabel2.Text = "Tên Bằng Cấp";
             // 
@@ -89,7 +111,7 @@
             // 
             // 
             this.txtMabangcap.CustomButton.Image = null;
-            this.txtMabangcap.CustomButton.Location = new System.Drawing.Point(92, 1);
+            this.txtMabangcap.CustomButton.Location = new System.Drawing.Point(151, 1);
             this.txtMabangcap.CustomButton.Name = "";
             this.txtMabangcap.CustomButton.Size = new System.Drawing.Size(21, 21);
             this.txtMabangcap.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
@@ -98,7 +120,7 @@
             this.txtMabangcap.CustomButton.UseSelectable = true;
             this.txtMabangcap.CustomButton.Visible = false;
             this.txtMabangcap.Lines = new string[0];
-            this.txtMabangcap.Location = new System.Drawing.Point(122, 36);
+            this.txtMabangcap.Location = new System.Drawing.Point(173, 47);
             this.txtMabangcap.MaxLength = 32767;
             this.txtMabangcap.Name = "txtMabangcap";
             this.txtMabangcap.PasswordChar = '\0';
@@ -107,7 +129,7 @@
             this.txtMabangcap.SelectionLength = 0;
             this.txtMabangcap.SelectionStart = 0;
             this.txtMabangcap.ShortcutsEnabled = true;
-            this.txtMabangcap.Size = new System.Drawing.Size(114, 23);
+            this.txtMabangcap.Size = new System.Drawing.Size(173, 23);
             this.txtMabangcap.TabIndex = 1;
             this.txtMabangcap.UseSelectable = true;
             this.txtMabangcap.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
@@ -116,9 +138,10 @@
             // metroLabel1
             // 
             this.metroLabel1.AutoSize = true;
-            this.metroLabel1.Location = new System.Drawing.Point(26, 40);
+            this.metroLabel1.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.metroLabel1.Location = new System.Drawing.Point(44, 51);
             this.metroLabel1.Name = "metroLabel1";
-            this.metroLabel1.Size = new System.Drawing.Size(90, 19);
+            this.metroLabel1.Size = new System.Drawing.Size(115, 25);
             this.metroLabel1.TabIndex = 0;
             this.metroLabel1.Text = "Mã Bằng Cấp";
             // 
@@ -143,56 +166,6 @@
             this.groupBox2.TabIndex = 0;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Thông tin bằng cấp";
-            // 
-            // btnThem
-            // 
-            this.btnThem.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnThem.Location = new System.Drawing.Point(556, 257);
-            this.btnThem.Name = "btnThem";
-            this.btnThem.Size = new System.Drawing.Size(75, 31);
-            this.btnThem.TabIndex = 3;
-            this.btnThem.Text = "Thêm";
-            this.btnThem.UseVisualStyleBackColor = true;
-            // 
-            // btnXoa
-            // 
-            this.btnXoa.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnXoa.Location = new System.Drawing.Point(718, 257);
-            this.btnXoa.Name = "btnXoa";
-            this.btnXoa.Size = new System.Drawing.Size(75, 31);
-            this.btnXoa.TabIndex = 3;
-            this.btnXoa.Text = "Xóa";
-            this.btnXoa.UseVisualStyleBackColor = true;
-            // 
-            // btnSua
-            // 
-            this.btnSua.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSua.Location = new System.Drawing.Point(799, 257);
-            this.btnSua.Name = "btnSua";
-            this.btnSua.Size = new System.Drawing.Size(75, 31);
-            this.btnSua.TabIndex = 3;
-            this.btnSua.Text = "Sửa";
-            this.btnSua.UseVisualStyleBackColor = true;
-            // 
-            // btnDong
-            // 
-            this.btnDong.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnDong.Location = new System.Drawing.Point(880, 257);
-            this.btnDong.Name = "btnDong";
-            this.btnDong.Size = new System.Drawing.Size(75, 31);
-            this.btnDong.TabIndex = 3;
-            this.btnDong.Text = "Đóng";
-            this.btnDong.UseVisualStyleBackColor = true;
-            // 
-            // btnLuu
-            // 
-            this.btnLuu.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnLuu.Location = new System.Drawing.Point(637, 257);
-            this.btnLuu.Name = "btnLuu";
-            this.btnLuu.Size = new System.Drawing.Size(75, 31);
-            this.btnLuu.TabIndex = 3;
-            this.btnLuu.Text = "Lưu";
-            this.btnLuu.UseVisualStyleBackColor = true;
             // 
             // dgvBangCap
             // 
@@ -236,6 +209,72 @@
             this.dgvBangCap.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dgvBangCap.Size = new System.Drawing.Size(937, 220);
             this.dgvBangCap.TabIndex = 2;
+            this.dgvBangCap.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvBangCap_CellClick);
+            // 
+            // btnThem
+            // 
+            this.btnThem.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThem.Location = new System.Drawing.Point(475, 257);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(75, 31);
+            this.btnThem.TabIndex = 3;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXoa.Location = new System.Drawing.Point(637, 257);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(75, 31);
+            this.btnXoa.TabIndex = 3;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnSua
+            // 
+            this.btnSua.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSua.Location = new System.Drawing.Point(718, 257);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(75, 31);
+            this.btnSua.TabIndex = 3;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.UseVisualStyleBackColor = true;
+            // 
+            // btnDong
+            // 
+            this.btnDong.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDong.Location = new System.Drawing.Point(880, 257);
+            this.btnDong.Name = "btnDong";
+            this.btnDong.Size = new System.Drawing.Size(75, 31);
+            this.btnDong.TabIndex = 3;
+            this.btnDong.Text = "Đóng";
+            this.btnDong.UseVisualStyleBackColor = true;
+            this.btnDong.Click += new System.EventHandler(this.btnDong_Click);
+            // 
+            // btnLuu
+            // 
+            this.btnLuu.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLuu.Location = new System.Drawing.Point(556, 257);
+            this.btnLuu.Name = "btnLuu";
+            this.btnLuu.Size = new System.Drawing.Size(75, 31);
+            this.btnLuu.TabIndex = 3;
+            this.btnLuu.Text = "Lưu";
+            this.btnLuu.UseVisualStyleBackColor = true;
+            this.btnLuu.Click += new System.EventHandler(this.btnLuu_Click);
+            // 
+            // btnHuy
+            // 
+            this.btnHuy.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHuy.Location = new System.Drawing.Point(799, 257);
+            this.btnHuy.Name = "btnHuy";
+            this.btnHuy.Size = new System.Drawing.Size(75, 31);
+            this.btnHuy.TabIndex = 3;
+            this.btnHuy.Text = "Hủy";
+            this.btnHuy.UseVisualStyleBackColor = true;
+            this.btnHuy.Click += new System.EventHandler(this.btnHuy_Click);
             // 
             // frmBangCap
             // 
@@ -243,6 +282,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(973, 540);
             this.Controls.Add(this.btnDong);
+            this.Controls.Add(this.btnHuy);
             this.Controls.Add(this.btnSua);
             this.Controls.Add(this.btnXoa);
             this.Controls.Add(this.btnLuu);
@@ -252,6 +292,7 @@
             this.Controls.Add(this.groupBox1);
             this.Name = "frmBangCap";
             this.Text = "QUẢN LÍ BẰNG CẤP";
+            this.Load += new System.EventHandler(this.frmBangCap_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
@@ -274,7 +315,8 @@
         private MetroFramework.Controls.MetroTextBox txtMabangcap;
         private MetroFramework.Controls.MetroLabel metroLabel1;
         private System.Windows.Forms.Button btnLuu;
-        private MetroFramework.Controls.MetroComboBox CbbTenbangcap;
         private MetroFramework.Controls.MetroGrid dgvBangCap;
+        private MetroFramework.Controls.MetroTextBox txtTenBangCap;
+        private System.Windows.Forms.Button btnHuy;
     }
 }
